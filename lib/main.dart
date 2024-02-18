d9344288dd0d968e98577a64520b6b2cea49fef6
prathambhandari <prathamkrishnabhandari@gmail.com>
2024-02-18 12:17:03 +0530

    added screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: StartScreen(),
+        body: StartScreen(
+          [Color.fromARGB(255, 36, 6, 88), Color.fromARGB(255, 48, 5, 122)],
+        ),
       ),
     ),
   );
-}
-
-class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 36, 6, 88),
-            Color.fromARGB(255, 48, 5, 122)
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/quiz-logo.png',
-              width: 300,
-            ),
-            const SizedBox(height: 50),
-            const Text(
-              'Learn flutter in fun way',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-              ),
-            ),
-            const SizedBox(height: 50),
-            ElevatedButton(onPressed: () {}, child: const Text('Start Quiz'))
-          ],
-        ),
-      ),
-    );
-  }
 }
